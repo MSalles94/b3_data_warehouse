b7bca0e4d0083f47b0cccc547bb1b9331b5db3e3
Matheus Salles CW <ahd10zz@mmm.com>
2026-07-02 14:27:02 -0300

    changes in code, transform market_summary

---
+++ scripts/map_market_summary.py
@@ -0,0 +1,46 @@
+map_market_summary={
+  
+    "table_0":{  
+        "comment":["sample table"],
+        "original_table_name":"",
+        "new_table_name":"0",
+        "table_header":[]
+    },
+    "table_1":{  
+        "comment":[""],
+        "original_table_name":"Ações - resumo das operações",
+        "new_table_name":"1",
+        "table_header":[]
+    },
+    "table_2":{  
+        "comment":[""],
+        "original_table_name":"Comportamento dos valores de referência das cotas dos ETFs (IOPV)",
+        "new_table_name":"2",
+        "table_header":[]
+    },
+    "table_3":{  
+        "comment":[""],
+        "original_table_name":"Mercado a termo",
+        "new_table_name":"3",
+        "table_header":[]
+    },
+    "table_4":{  
+        "comment":[""],
+        "original_table_name":"Negociação de estratégias",
+        "new_table_name":"4",
+        "table_header":[]
+    },
+    "table_5":{  
+        "comment":[""],
+        "original_table_name":"Negócios consolidados do pregão",
+        "new_table_name":"5",
+        "table_header":[]
+    },
+    "table_6":{  
+        "comment":[""],
+        "original_table_name":"Negócios consolidados do pregão não regular",
+        "new_table_name":"6",
+        "table_header":[]
+    }
+ 
+}
